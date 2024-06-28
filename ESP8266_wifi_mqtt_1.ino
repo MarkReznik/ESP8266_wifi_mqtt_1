198aa4326e80d09fe37c4d5e77a698dc6c067632
MarkReznik <mark.reznik77@gmail.com>
2024-06-28 09:02:39 +0300

    Initial commit

---
+++ ESP8266_wifi_mqtt_1.ino
@@ -0,0 +1,140 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <RCSwitch.h>
+#include <PubSubClient.h>
+
+// MQTT Broker settings
+const char *mqtt_broker = "f16e3b17.ala.asia-southeast1.emqxsl.com";  // EMQX broker endpoint
+const char *mqtt_topic = "test/1";  // MQTT topic
+const char *mqtt_username = "mark";  // MQTT username for authentication
+const char *mqtt_password = "123456";  // MQTT password for authentication
+const int mqtt_port = 8883;  //8883;//1883;  // MQTT port (TCP)
+
+//WiFiClient espClient;
+WiFiClientSecure espClient;     // <-- Change #1: Secure connection to MQTT Server
+
+// Create an instance of WiFiClientSecure
+WiFiClientSecure wifiClient;
+// Create an instance of PubSubClient
+PubSubClient mqtt_client(espClient);
+
+/*
+static const char *fingerprint PROGMEM = "44 14 9A 3F C3 E9 F1 F3 84 1A B4 9F B6 4D 19 8A B2 92 31 D6";
+
+// Root CA certificate
+const char* rootCACertificate = R"(
+-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----
+)";
+*/
+
+void connectToMQTTBroker();
+
+void mqttCallback(char *topic, byte *payload, unsigned int length);
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+    // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    
+    mySwitch.enableReceive(D2);  // Receiver on interrupt 0 => that is pin #2
+    
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    // wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
+
+    bool res;
+    // res = wm.autoConnect(); // auto generated AP name from chipid
+     res = wm.autoConnect("192-168-4-1"); // anonymous ap
+    //wm.erase();
+    //res = wm.autoConnect("ip192-168-4-1_p1-8","12345678"); // password protected ap
+    //wm.erase()
+    if(!res) {
+        Serial.println("Failed to connect");
+        // ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+        //espClient.setFingerprint(fingerprint);   // <-- Change #3: Set the SHA1 fingerprint
+        espClient.setInsecure();// Alternative: 
+        mqtt_client.setServer(mqtt_broker, mqtt_port);
+        mqtt_client.setCallback(mqttCallback);
+        
+        // Load root CA certificate into WiFiClientSecure object
+        //wifiClient.setTrustAnchors(new BearSSL::X509List(rootCACertificate));
+
+        connectToMQTTBroker();
+    }
+
+}
+void connectToMQTTBroker() {
+    while (!mqtt_client.connected()) {
+        String client_id = "esp8266-client-" + String(WiFi.macAddress());
+        Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
+        if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Connected to MQTT broker");
+            mqtt_client.subscribe(mqtt_topic);
+            // Publish message upon successful connection
+            mqtt_client.publish(mqtt_topic, "Hi EMQX I'm ESP8266 ^^");
+        } else {
+            Serial.print("Failed to connect to MQTT broker, rc=");
+            Serial.print(mqtt_client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+void mqttCallback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message received on topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (unsigned int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:  
+    if (mySwitch.available()) {
+      //output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+      mySwitch.resetAvailable();
+    } 
+    if (!mqtt_client.connected()) {
+        connectToMQTTBroker();
+    }
+    mqtt_client.loop();
+}
